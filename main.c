e4b8f73c320b9657bad9f554d04d9b3e9df72223
Djiraiya <p4g4n4ig4ng@gmail.com>
2017-02-22 17:12:18 +0700

    Second

---
+++ main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+void dohod (int v, int s)
+{	
+	if (v<=100000) {if (s<=30)
+	v=v-v/10;}
+	if (v>100000) {if (s<=30)
+    v=v-v/10;}
+	
+	if (v<=100000) 
+	if (s>30 && s<=120)
+	v=v+v*0.02;
+	if (s>120 && s<=240)
+	v=v+v*0.06;
+	if (s>240 && s<=365)
+	v=v+v*0.12;
+	{
+	if (v>100000)
+	if (s>30 && s<=120)
+	v=v+v*0.03;
+	if (s>120 && s<=240)
+	v=v+v*0.08;
+	if (s>240 && s<=365)
+	v=v+v*0.15;}
+	
+	if (v<10000) {printf("\nMinimalnaya symma vklada 10000rub\n");}
+	else printf("\nDohod=%d",v);
+	if (s<0 || s>365) {printf(" Nekorektnii srok");}
+}
+
+int main()
+{
+  int c,d,v;
+  printf("Vvedite symmy vklada i srok\n");
+  scanf("%d %d",&c,&d);
+  printf("Vklad:%d Srok:%d",c,d);
+  
+  dohod (c,d);
+ 
+}
