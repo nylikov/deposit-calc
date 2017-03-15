43108194ea3c19961e0c2c19d5954c684ab3de80
Djiraiya <p4g4nig4ng@gmail.com>
2017-03-15 20:04:57 +0700

    Fix code

--- main.c
+++ main.c
@@ -2,18 +2,19 @@
 
 void dohod (int v, int s)
 {	
+	printf("Vklad:%d Srok:%d",v,s);
+	{
 	if (v<=100000) {if (s<=30)
 	v=v-v/10;}
 	if (v>100000) {if (s<=30)
     v=v-v/10;}
-	
 	if (v<=100000) 
 	if (s>30 && s<=120)
 	v=v+v*0.02;
 	if (s>120 && s<=240)
 	v=v+v*0.06;
 	if (s>240 && s<=365)
-	v=v+v*0.12;
+	v=v+v*0.12;}
 	{
 	if (v>100000)
 	if (s>30 && s<=120)
@@ -22,19 +23,16 @@ void dohod (int v, int s)
 	v=v+v*0.08;
 	if (s>240 && s<=365)
 	v=v+v*0.15;}
-	
-	if (v<10000) {printf("\nMinimalnaya symma vklada 10000rub\n");}
-	else printf("\nDohod=%d",v);
-	if (s<0 || s>365) {printf(" Nekorektnii srok");}
+	printf("\nDohod=%d\n", v);
 }
-
 int main()
-{
-  int c,d;
-  printf("Vvedite symmy vklada i srok\n");
-  scanf("%d %d",&c,&d);
-  printf("Vklad:%d Srok:%d",c,d);
-  
-  dohod (c,d);
- 
+{	
+	int c,d;
+	printf("Vvedite symmy vklada\n");
+	scanf("%d",&c);
+	if (c<10000) {printf("\nMinimalnaya symma vklada 10000rub\n");return 1;}
+	printf("Vvedite srok\n");
+	scanf("%d",&d);
+	if (d<0 || d>365) {printf("Nekorektnii srok");return 1;}	
+	else dohod (c,d);
 }
